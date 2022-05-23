0ef3c69f6759789e4d4cf77fbfa2555328bd99c5
Kyle <ktvitale7@gmail.com>
2022-05-23 19:42:32 -0400

    commit tree 42bd30fa6286c8000902d13f1535b3580b3b2cfe

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+
+function submitData(name, email){
+    const userObj = {
+        name: name,
+        email: email
+    }
+    fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(userObj)
+    })
+    .then(res => res.json())
+    .then(userObj => document.body.innerHTML = userObj.id)
+    .catch((error) => document.body.innerHTML = error.message)
+}
+
